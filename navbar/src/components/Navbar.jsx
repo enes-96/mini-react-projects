2d585aa083e10d8c8baf3a003ba7cb554931b4c4
Enes <seyit7enes@gmail.com>
2023-05-28 22:04:57 +0200

    commit

--- navbar/src/components/Navbar.jsx
+++ navbar/src/components/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { SiReact } from "react-icons/si";
+import { HiOutlineMenu } from "react-icons/hi";
 
 const Navbar = () => {
+  const [mobileScreen, setMobileScreen] = useState(false);
+
+  useEffect(() => {
+    function handleMobileScreen() {
+      setMobileScreen(window.innerWidth >= 768);
+    }
+
+    handleMobileScreen();
+
+    window.addEventListener("resize", handleMobileScreen);
+
+    return () => {
+      window.removeEventListener("resize", handleMobileScreen);
+    };
+  }, []);
+
+  const navItems = [
+    { label: "Home", path: "/" },
+    { label: "Services", path: "/" },
+    { label: "Products", path: "/" },
+    { label: "Contact Us", path: "/" },
+  ];
+
   return (
     <div>
-      <header className="w-full h-2 bg-emerald-200 "></header>
+      <header className="w-full bg-sky-500 flex items-center justify-between p-4 text-white">
+        <SiReact className="text-4xl text-sky-900" />
+        {mobileScreen ? (
+          <nav>
+            <ul className="flex gap-1 text-xl items-center cursor-pointer">
+              {navItems.map((item, index) => (
+                <li key={index} className="py-1 px-7">
+                  {item.label}
+                </li>
+              ))}
+              <li className="bg-sky-800 py-1 px-7 rounded-md">Sign Up</li>
+            </ul>
+          </nav>
+        ) : (
+          <nav>
+            <HiOutlineMenu className="text-3xl cursor-pointer" />
+          </nav>
+        )}
+      </header>
     </div>
   );
 };
