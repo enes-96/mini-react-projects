7893aeba39595a2162659bbdfabc5f6776f3acfc
Enes <seyit7enes@gmail.com>
2023-05-28 20:30:23 +0200

    setup react tailwind

---
+++ navbar/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <header className="w-full h-2 bg-emerald-200 "></header>
+    </div>
+  );
+};
+
+export default Navbar;
