c6e1dce3efd39b1953672c0060e0dec37175c355
Enes <seyit7enes@gmail.com>
2023-06-01 19:22:01 +0200

    setup

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,36 +1,9 @@
-import React, { useState } from "react";
-import Display from "./components/Display";
-import Button from "./components/Button";
-
-const btnValues = [
-  ["C", "+-", "%", "/"],
-  [7, 8, 9, "X"],
-  [4, 5, 6, "-"],
-  [1, 2, 3, "+"],
-  [0, ".", "="],
-];
-
+import React from "react";
 const App = () => {
-  const [firstNum, setFirstNum] = useState([]);
-
-  const handleFirstNum = (val) => {
-    setFirstNum([...firstNum, val]);
-  };
-
   return (
     <div>
-      <Display value={firstNum} />
-      <div className="grid grid-cols-3">
-        {btnValues.flat().map((value, index) => (
-          <Button
-            onClick={() => handleFirstNum(value)}
-            key={index}
-            value={value}
-          />
-        ))}
-      </div>
+      <h1>hi from app</h1>
     </div>
   );
 };
-
 export default App;
--- calculator/src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ value, onClick }) => {
-  return <button onClick={onClick}>{value}</button>;
-};
-
-export default Button;
