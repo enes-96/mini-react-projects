130b1a6b5c6bb06e670b149aa973786ced873acc
Enes <seyit7enes@gmail.com>
2023-06-12 18:24:35 +0200

    commit

--- responsive-website/src/components/home/FaqSection.jsx
+++ responsive-website/src/components/home/FaqSection.jsx
@@ -5,6 +5,11 @@ const FaqSection = () => {
     <div className=" max-w-screen-2xl mx-auto my-10  ">
       <div className="border-2 ">
         <h3 className="text-center text-5xl ">Most Asked</h3>
+        <div>
+          <div>
+            <h4>Can i use vpn with multiple devices</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
