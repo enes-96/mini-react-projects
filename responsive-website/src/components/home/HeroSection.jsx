8cd0ace6ef9a3e83a6b012f441f36344b1e6aa5b
Enes <seyit7enes@gmail.com>
2023-06-20 23:08:10 +0200

    commit

--- responsive-website/src/components/home/HeroSection.jsx
+++ responsive-website/src/components/home/HeroSection.jsx
@@ -3,13 +3,15 @@ import ipohneImg from "../../assets/images/ipohne.png";
 
 const HeroSection = () => {
   return (
-    <div className="border-2 border-green-400">
-      <div className="flex justify-between min-h-screen py-10">
-        <div>
-          <h1>Unleash Online Freedom with Lynx</h1>
+    <div className="border-2 ">
+      <div className="flex justify-between min-h-screen py-10 ">
+        <div className="">
+          <h1 className=" text-5xl">Unleash Online Freedom with Lynx</h1>
         </div>
-        <div id="heroIpohneBg" className="">
-          <h1>ipone</h1>
+        <div className="w-1/2 grid place-items-center">
+          <div className="w-80">
+            <img src={ipohneImg} className=" w-80" />
+          </div>
         </div>
       </div>
     </div>
