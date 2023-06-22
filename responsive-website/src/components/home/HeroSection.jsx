5016f13a79df7b9caaf0232dd0fed753030fbcc7
Enes <seyit7enes@gmail.com>
2023-06-22 23:09:05 +0200

    hero styling

--- responsive-website/src/components/home/HeroSection.jsx
+++ responsive-website/src/components/home/HeroSection.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import ipohneImg from "../../assets/images/ipohne.png";
+import { HiOutlineDownload } from "react-icons/hi";
 
 const HeroSection = () => {
   return (
-    <div className="border-2 ">
-      <div className="flex justify-between min-h-screen py-10 ">
+    <div className="">
+      <div className="grid grid-cols-2  min-h-screen py-10 ">
         <div className="">
-          <h1 className=" text-5xl">Unleash Online Freedom with Lynx</h1>
+          <h1 className=" text-5xl mt-20 text-center">
+            Unleash Online Freedom with Lynx
+          </h1>
+          <p className="w-3/4 text-xl my-4 leading-relaxed">
+            Enjoy the full functionality of LynxVPN for 30 days without any
+            obligation! Choose any plan and try the best VPN now - completely
+            risk-free. Not completely satisfied? Cancel at any time within the
+            first 30 days and get your money back. And completely.
+          </p>
+          <div className="bg-black text-white flex items-center w-min p-4  rounded-xl gap-2 text-lg">
+            <HiOutlineDownload />
+            <p>download</p>
+          </div>
         </div>
-        <div className="w-1/2 grid place-items-center">
+        <div className="w-1/2 grid place-items-center place-self-center ">
           <div className="w-80">
-            <img src={ipohneImg} className=" w-80" />
+            <img src={ipohneImg} className=" w-96" />
           </div>
         </div>
       </div>
