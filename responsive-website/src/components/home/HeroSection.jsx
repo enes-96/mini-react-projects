7000370fe926f75a50cf0a32a94584b07595af6c
enes <seyit7enes@gmail.com>
2023-07-01 19:47:41 +0200

    phone desing

--- responsive-website/src/components/home/HeroSection.jsx
+++ responsive-website/src/components/home/HeroSection.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { HiOutlineDownload } from "react-icons/hi";
+import logo from "../../assets/images/lynxhead.png";
+import {
+  HiOutlineDownload,
+  HiOutlineChevronDown,
+  HiOutlineChevronUp,
+} from "react-icons/hi";
 import {
   HiMiniPlayCircle,
   HiOutlineBars3,
   HiOutlineUser,
 } from "react-icons/hi2";
-import logo from "../../assets/images/lynxhead.png";
-
 import {
   SiBbc,
   SiSony,
@@ -57,11 +59,47 @@ const HeroSection = () => {
           </div>
         </div>
         <div className="w-1/2 grid place-items-center place-self-center ">
-          <div className="rounded-3xl px-7 pt-14" id="phoneFrame">
-            <div className="flex items-center  justify-between text-2xl my-2">
+          <div
+            className="rounded-3xl px-7 pt-14 relative overflow-hidden"
+            id="phoneFrame"
+          >
+            <div className="flex items-center  justify-between text-2xl mt-3 mb-5">
               <HiOutlineBars3 className="" />
               <img src={logo} alt="" className=" h-8" />
               <HiOutlineUser className=" fill-black" />
+            </div>
+            <p className="text-center my-3 text-2xl text-green-900 font-bold">
+              Connected
+            </p>
+            <p className="text-center ">Tokyo, Japan</p>
+            <p className=" mb-5 text-center text-xs">11.240.222.128</p>
+            <div className=" bg-neutral-300 h-32  w-16 rounded-xl m-auto my-5 ">
+              <div className=" h-16 w-full grid place-items-center bg-neutral-700  rounded-xl">
+                <p className="text-green-700 font-bold">ON</p>
+              </div>
+            </div>
+            <div className="flex w-52 mx-auto mt-7 mb-5 items-center justify-between">
+              <div className="flex text-xs gap-2">
+                <div className="bg-neutral-300 h-8 grid place-items-center w-8 rounded-md text-xl">
+                  <HiOutlineChevronDown className="text-green-700 " />
+                </div>
+                <div>
+                  <p>Download</p>
+                  <p>34.2 Mbps</p>
+                </div>
+              </div>
+              <div className="flex text-xs gap-2">
+                <div className="bg-neutral-300 h-8 grid place-items-center w-8 rounded-md text-xl">
+                  <HiOutlineChevronUp className="text-orange-700 " />
+                </div>
+                <div>
+                  <p>Upload</p>
+                  <p>19.7 Mbps</p>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <p className="text-center">Available Servers</p>
             </div>
           </div>
         </div>
